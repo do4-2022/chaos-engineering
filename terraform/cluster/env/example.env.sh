fcb2c9dc48728e0fcf77eec49ad22252e33ad48e
RedbeanGit <dubois.julien.mail@gmail.com>
2024-04-17 16:03:04 +0200

    fix: typo in tf env var
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- terraform/cluster/env/example.env.sh
+++ terraform/cluster/env/example.env.sh
@@ -5,8 +5,8 @@
 export TF_VAR_nb_master=1
 export TF_VAR_nb_worker=1
 
-export TF_VAR_public_ssh_key_path="$HOME/.ssh/openstack.pub"
-export TF_VAR_private_ssh_key_path="$HOME/.ssh/openstack"
+export TF_VAR_ssh_public_key_path="$HOME/.ssh/openstack.pub"
+export TF_VAR_ssh_private_key_path="$HOME/.ssh/openstack"
 
 export TF_VAR_kubeconfig_path="$HOME/.kube/config"
 
