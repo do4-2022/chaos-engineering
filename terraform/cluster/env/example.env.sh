a05be6e9630368cd898119a7841ff99b58b54871
RedbeanGit <dubois.julien.mail@gmail.com>
2024-04-17 15:59:43 +0200

    fix: remove useless env variables
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- terraform/cluster/env/example.env.sh
+++ terraform/cluster/env/example.env.sh
@@ -2,8 +2,6 @@
 
 #= MODIFY THESE LINES =============================================================================
 
-export CLUSTER_NAME=""
-
 export TF_VAR_nb_master=1
 export TF_VAR_nb_worker=1
 
@@ -14,6 +12,4 @@ export TF_VAR_kubeconfig_path="$HOME/.kube/config"
 
 #= DO NOT MODIFY THESE LINES ======================================================================
 
-export STATE_FILE="states/${CLUSTER_NAME}.tfstate"
-echo "Environment variables loaded (environment: $TF_VAR_environment)"
 echo "Use 'terraform init' to initialize Terraform with the correct backend configuration"
