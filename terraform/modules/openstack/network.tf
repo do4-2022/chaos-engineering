e3480157e88b041a782130c03b79af75871ff75a
RedbeanGit <dubois.julien.mail@gmail.com>
2024-05-21 16:06:09 +0200

    fix: allow http traffic
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- terraform/modules/openstack/network.tf
+++ terraform/modules/openstack/network.tf
@@ -58,3 +58,22 @@ resource "openstack_networking_secgroup_rule_v2" "security_group_rule_internal" 
   remote_group_id   = openstack_networking_secgroup_v2.secgroup.id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "security_group_rule_http" {
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "security_group_rule_https" {
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+}
