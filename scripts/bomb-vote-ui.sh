3984058124e2f2be77cf138f9b68c629e3e9476b
RedbeanGit <dubois.julien.mail@gmail.com>
2024-05-21 09:07:22 +0200

    fix: prevent port forward conflicts
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- scripts/bomb-vote-ui.sh
+++ scripts/bomb-vote-ui.sh
@@ -5,5 +5,5 @@ BINARY=hey_linux_amd64
 kubectl port-forward svc/vote-ui 31000:80 -n votingapp
 kubectl port-forward svc/vote-ui 31010:80 -n votingapp-nats
 
-./$BINARY -z 3m -m GET http://localhost:31000
-./$BINARY -z 3m -m GET http://localhost:31010
+./$BINARY -z 3m -m POST http://localhost:31000 -d '{"vote": "a","voter_id": ""}'
+./$BINARY -z 3m -m POST http://localhost:31010 -d '{"vote": "a","voter_id": ""}'
