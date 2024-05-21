5572c3c3a3298e3a8bfce9073d9234d372853750
RedbeanGit <dubois.julien.mail@gmail.com>
2024-05-21 09:51:17 +0200

    fix: update script
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- scripts/bomb-vote-ui.sh
+++ scripts/bomb-vote-ui.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-BINARY=hey_linux_amd64
+BINARY="./scripts/bin/hey_linux_amd64"
 
-kubectl port-forward svc/vote-ui 31000:80 -n votingapp
-kubectl port-forward svc/vote-ui 31010:80 -n votingapp-nats
+export KUBECONFIG="terraform/modules/k0s/output/kubeconfig.yaml"
+kubectl port-forward svc/vote-ui 31000:80 -n votingapp &
+kubectl port-forward svc/vote-ui 31010:80 -n votingapp-nats &
 
-./$BINARY -z 3m -m POST http://localhost:31000 -d '{"vote": "a","voter_id": ""}'
-./$BINARY -z 3m -m POST http://localhost:31010 -d '{"vote": "a","voter_id": ""}'
+$BINARY -z 3m -m POST http://localhost:31000 -d '{"vote": "a","voter_id": ""}' &
+$BINARY -z 3m -m POST http://localhost:31010 -d '{"vote": "a","voter_id": ""}' &
+
+wait
